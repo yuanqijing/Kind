4eb730f50b0c044265ea00a41632834b56f91cf9
yuan <yuanqijing00x@gmail.com>
2022-04-23 14:02:45 +0800

    add cilium ui configuration.

---
+++ Cilium/readme.md
@@ -0,0 +1,9 @@
+reference:
+https://medium.com/@charled.breteche/kind-cluster-with-cilium-and-no-kube-proxy-c6f4d84b5a9d
+
+kind cluster
+Cilium pods & Cilium Controller
+ingress controller
+    It requires configuring the cluster spec to expose additional ports from the host computer and let our ingress controller receive traffic:
+
+Cilium Hubble UI
--- Cilium/up.sh
+++ Cilium/up.sh
@@ -1,5 +1,7 @@
-kind create cluster --config cluster.yaml
+kind delete cluster && kind create cluster --config cluster.yaml
 
 helm repo update
 
 helm upgrade --install --namespace kube-system --repo https://helm.cilium.io cilium cilium --values value.yaml
+
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.1.1/deploy/static/provider/kind/deploy.yaml
